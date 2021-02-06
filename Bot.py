073e9d5bf16f5dc90f0d2c1ddfcc30b86e9e025e
Bianca <bianca_uwu@yahoo.com>
2021-02-06 17:34:05 +0100

    FIX #6
    - Changed a word because some child raged at me for it being 'offensive'

--- Bot.py
+++ Bot.py
@@ -5,7 +5,7 @@
 # This was created because a lot of script kiddies have been making poorly coded Discord DDoS bots, they used requests.get/post for Discord Bots, they used unclean code,
 # they used poor grammar on the bots, and so on.
 #
-# With poor grammar, I refer to the little school-drop-outs that say 'your' instead of 'you're', 'i' instead of 'I', 'dont' instead of 'don't', and so on.
+# With poor grammar, I refer to the people that say 'your' instead of 'you're', 'i' instead of 'I', 'dont' instead of 'don't', and so on.
 # These little children should pay attention in school. Such grammar mistakes make you look more silly, and therefore you will archive less.
 #
 # This was made by XxBiancaXx#4356.
